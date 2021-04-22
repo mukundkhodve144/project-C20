d826330a59735b22f9b60055dbe505dc7e3b5b3a
mukundkhodve144 <76438877+mukundkhodve144@users.noreply.github.com>
2021-04-22 14:33:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,25 +10,52 @@ function setup() {
 
   speed = random(55, 90);
   weight = random(400, 1500);
-}
-
-function draw() {
-  background(255, 255, 255);  
 
   car.velocityX = speed;
 
-  if(wall.x-car.x < (car.width+wall.width/2)){
-    car.velocityX = 0;
-    var  drformation = 0.5* weight* speed* speed/2250;
-    if(drformation>180){
-      car.shapeColor = color(255, 0, 0);
-    }
-    if(deformation>100 && deformation<180){
-      car.shapeColor = color(230, 230, 0);
-    }
-    if(deformation<100){
-      car.shapeColor = color(0, 255, 0);
-    }
-  }
+}
+
+function draw() {
+  background(0);  
+
+   if(car.collide(wall)){
+     deformation();
+     car.velocityX = 0;
+   }
+  
+  fill("yellow")
+  textSize(19);
+  text("deformation:"+Math.round((0.5* weight* speed* speed)/22500),1150,350);
+
+fill("red")
+textSize(19);
+text("deformation>=180 is considered to be dangerous for passengers",50,310);
+
+fill("yellow")
+textSize(19);
+text("deformation between 80 to 180 is considered to be average for passengers",50,350);
+
+fill("green")
+textSize(19);
+text("deformation < 80 is considered to be good for passengers",50,270);
+
+
   drawSprites();
+}
+
+function deformation () {
+
+  var deformation = (0.5* weight* speed* speed)/22500;
+  if(deformation>180){
+    car.shapeColor = color(255, 0, 0);
+  }
+
+  if(deformation<180 && deformation>100){
+    car.shapeColor = color(230, 230, 0);
+  }
+
+  if(deformation<100){
+    car.shapeColor = color(0, 255, 0);
+  }
+  
 }
